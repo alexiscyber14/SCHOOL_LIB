7a9687eae7b1229bac5790ba14d171e6843731f1
alexiscyber14 <142sledgehammer14@gmail.com>
2023-05-22 19:28:46 +0100

    Add rental and classroom classes and their relationships

--- person.rb
+++ person.rb
@@ -1,18 +1,17 @@
 require './nameable'
+require './book'
+require './rental'
+require './classroom'
 
 class Person < Nameable
-  attr_accessor :id, :name, :age
+  attr_accessor :id, :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super(name)
     @id = Random.rand(1...1000)
-    super()
-    @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def correct_name
-    @name
+    @rentals = []
   end
 
   def can_use_services?
